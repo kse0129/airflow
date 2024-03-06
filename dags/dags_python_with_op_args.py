88a103087862cf54281a8a9c39aab296c3eaab79
kse <seongeon1999@naver.com>
2024-03-06 17:16:19 +0900

    upload operator with op_args

---
+++ dags/dags_python_with_op_args.py
@@ -0,0 +1,19 @@
+from airflow.models.dag import DAG
+from airflow.operators.python import PythonOperator
+from common.common_python import regist
+
+import datetime
+import pendulum
+import random
+
+with DAG(
+    dag_id="dags_python_with_op_args",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2024, 3, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    regist_t1 = PythonOperator(
+        task_id="regist_t1",
+        python_callable=regist,
+        op_args=["kse", "man", "KR", "Seoul"]
+    )
