ee5c88602a6f4c521dbef9a97f16c82fb0720e0f
kse <seongeon1999@naver.com>
2024-03-06 16:34:48 +0900

    Modify dag_id

--- dags/dags_python_import_operator.py
+++ dags/dags_python_import_operator.py
@@ -8,14 +8,14 @@ from common.common_python import get_sftp
 
 
 with DAG(
-    dag_id="dags_python_operator",
+    dag_id="dags_python_import_operator",
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2024, 3, 1, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
-    py_t1 = PythonOperator(
-        task_id="py_t1",
+    task_get_sftp = PythonOperator(
+        task_id="task_get_sftp",
         python_callable=get_sftp
     )
 
-    py_t1
+    task_get_sftp
