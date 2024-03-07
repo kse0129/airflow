ef358408d8f3f797d14ec3a1c4877c3a3d340726
kse <seongeon1999@naver.com>
2024-03-07 10:32:31 +0900

    upload show templates

---
+++ dags/dags_python_show_template.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+from airflow.decorators import task
+
+import pendulum
+import datetime
+
+with DAG(
+    dag_id="dags_python_show_templates",
+    schedule="30 9 * * *",
+    start_date=pendulum.datetime(2024, 2, 28, tz="Asia/Seoul")
+) as dag:
+    @task(task_id="python_task")
+    def show_templates(**kwargs):
+        from pprint import pprint
+        pprint(kwargs)
+
+    show_templates()
