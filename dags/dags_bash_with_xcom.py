bdd8fd09ba655b028d509193685a3f1995302838
kse <seongeon1999@naver.com>
2024-03-12 08:38:52 +0900

    modify dag id

--- dags/dags_bash_with_xcom.py
+++ dags/dags_bash_with_xcom.py
@@ -5,7 +5,7 @@ import datetime
 import pendulum
 
 with DAG(
-    dag_id="dags_bash_with_template",
+    dag_id="dags_bash_with_xcom",
     schedule="10 0 * * *",
     start_date=pendulum.datetime(2024, 3, 1, tz="Asia/Seoul"),
     catchup=False,
