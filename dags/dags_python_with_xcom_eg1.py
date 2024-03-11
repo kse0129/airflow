2412e6d5461283144bfe3138d772d6f492c69c89
kse <seongeon1999@naver.com>
2024-03-11 17:08:19 +0900

    upload data sharing by xcom

---
+++ dags/dags_python_with_xcom_eg1.py
@@ -0,0 +1,35 @@
+from airflow import DAG
+from airflow.decorators import task
+
+import pendulum
+import datetime
+
+with DAG(
+    dag_id="dags_python_with_xcom_eg1",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+    @task(task_id='python_xcom_push_task1')
+    def xcom_push1(**kwargs):
+        ti = kwargs['ti']
+        ti.xcom_push(key='result1', value='value_1')
+        ti.xcom_push(key='result2', value=[1, 2, 3])
+
+    @task(task_id='python_xcom_push_task2')
+    def xcom_push2(**kwargs):
+        ti = kwargs['ti']
+        ti.xcom_push(key='result1', value='value_2')
+        ti.xcom_push(key='result2', value=[1, 2, 3, 4])
+
+    @task(task_id='python_xcom_pull_task')
+    def xcom_pull(**kwargs):
+        ti = kwargs['ti']
+        value1 = ti.xcom_pull(key='result1')
+        value2 = ti.xcom_pull(key='result2', task_ids='python_xcom_push_task1')
+        print('---------------')
+        print(f'value1: {value1}')
+        print(f'value2: {value2}')
+
+    xcom_push1() >> xcom_push2() >> xcom_pull()
---
+++ dags/dags_python_with_xcom_eg2.py
@@ -0,0 +1,30 @@
+from airflow import DAG
+from airflow.decorators import task
+
+import pendulum
+import datetime
+
+with DAG(
+    dag_id="dags_python_with_xcom_eg1",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+    @task(task_id='python_xcom_push_by_return')
+    def xcom_push_result(**kwargs):
+        return 'success'
+    
+    @task(task_id='python_xcom_pull1')
+    def xcom_pull_1(**kwargs):
+        ti = kwargs['ti']
+        value1 = ti.xcom_pull(task_ids='python_xcom_push_by_return')
+        print(f'return value by xcom_pull directly: {value1}')
+
+    @task(task_id='python_xcom_pull2')
+    def xcom_pull_2(status, **kwargs):
+        print(f"return value by function parameter: {status}")
+
+    task_xcom_push = xcom_push_result()
+    xcom_pull_2(task_xcom_push)
+    task_xcom_push >> xcom_pull_1()
