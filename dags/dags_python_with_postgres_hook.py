c9a679756c21e42654081f302ecf47e3a9154d34
kse <seongeon1999@naver.com>
2024-03-14 14:12:31 +0900

    upload postgres hook

---
+++ dags/dags_python_with_postgres_hook.py
@@ -0,0 +1,33 @@
+from airflow import DAG
+import pendulum
+from airflow.operators.python import PythonOperator
+
+with DAG(
+    dag_id='dags_python_with_postgres_hook',
+    start_date=pendulum.datetime(2024, 3, 1, tz='Asia/Seoul'),
+    schedule=None,
+    catchup=False
+) as dag:
+    
+    def insert_postgres(postgres_conn_id, **kwargs):
+        from airflow.providers.postgres.hook.postgres import PostgresHook
+        from contextlib import closing
+
+        postgres_hook = PostgresHook(postgres_conn_id)
+        with closing(postgres_hook.get_conn()) as conn:
+            with closing(conn.cursor()) as cursor:
+                dag_id = kwargs.get('ti').dag_id
+                task_id = kwargs.get('ti').task_id
+                run_id = kwargs.get('ti').run_id
+                msg = 'insert 수행'
+                sql = 'INSERT INTO py_opr_drct_insrt values (%s, %s, %s, %s);'
+                cursor.execute(sql, (dag_id, task_id, run_id, msg))
+                conn.commit()
+
+    insert_postgres_with_hook = PythonOperator(
+        task_id='insert_postgres_with_hook',
+        python_callable=insert_postgres,
+        op_args={'postgres_conn_id':'conn-db-postgres-custom'}
+    )
+
+    insert_postgres_with_hook
