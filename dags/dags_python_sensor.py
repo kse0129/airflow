ed3a8bdeceeec0470459fbf9cf6331511a85c0c2
kse <seongeon1999@naver.com>
2024-03-15 13:37:03 +0900

    modify poke interval

--- dags/dags_python_sensor.py
+++ dags/dags_python_sensor.py
@@ -43,7 +43,7 @@ with DAG(
         python_callable=check_api_update,
         op_kwargs={
             'http_conn_id': 'openapi.seoul.go.kr',
-            'endpoint':'{{var.value.apikey_seoul_go_kr}}/json/TbCorona19CountStatus',
+            'endpoint':'{{var.value.apikey_openapi_seoul_go_kr}}/json/TbCorona19CountStatus',
             'base_dt_col':'S_DT'
         },
         poke_interval=30,
