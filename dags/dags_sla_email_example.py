268a38550921a6d272782e71deaaea6dc1937a91
kse <seongeon1999@naver.com>
2024-03-27 10:46:35 +0900

    add tasks

--- dags/dags_sla_email_example.py
+++ dags/dags_sla_email_example.py
@@ -9,6 +9,33 @@ email_list = [email.strip() for email in email_str.split(',')]
 
 with DAG(
     dag_id='dags_sla_email_example',
-    start_date=pendulum.datetime(2024, 3, 1, tz='Asia/Seoul')
+    start_date=pendulum.datetime(2024, 3, 1, tz='Asia/Seoul'),
+    schedule='*/10 * * * *',
+    catchup=False,
+    default_args={
+        'sla': timedelta(seconds=70),
+        'email': email_list
+    }
 ) as dag:
-    pass
+    
+    task_slp_30s_sla_70s = BashOperator(
+        task_id='task_slp_30s_sla_70s',
+        bash_command='sleep 30'
+    )
+
+    task_slp_60s_sla_70s = BashOperator(
+        task_id='task_slp_60s_sla_70s',
+        bash_command='sleep 60'
+    )
+
+    task_slp_10s_sla_70s = BashOperator(
+        task_id='task_slp_10s_sla_70s',
+        bash_command='sleep 10'
+    )
+
+    task_slp_10s_sla_30s = BashOperator(
+        task_id='task_slp_10s_sla_30s',
+        bash_command='sleep 10'
+    )
+
+    task_slp_30s_sla_70s >> task_slp_60s_sla_70s >> task_slp_10s_sla_70s >> task_slp_10s_sla_30s
