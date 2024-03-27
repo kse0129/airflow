29d9b7a62451250119566603e2cec5cdd3cbfa71
kse <seongeon1999@naver.com>
2024-03-27 14:25:33 +0900

    upload timeout examples

--- dags/dags_timeout_example_2.py
+++ dags/dags_timeout_example_2.py
@@ -9,24 +9,29 @@ email_str = Variable.get("email_target")
 email_list = [email.strip() for email in email_str.split(',')]
 
 with DAG(
-    dag_id='dags_timeout_example_1',
+    dag_id='dags_timeout_example_2',
     start_date=pendulum.datetime(2024, 3, 1, tz='Asia/Seoul'),
     catchup=False,
     schedule=None,
     dagrun_timeout=timedelta(minutes=1),
     default_args={
-        'execution_timeout': timedelta(seconds=20),
+        'execution_timeout': timedelta(seconds=40),
         'email_on_failure': True,
         'email': email_list
     }
 ) as dag:
-    bash_sleep_30 = BashOperator(
-        task_id='bash_sleep_30',
-        bash_command='sleep 30'
+    bash_sleep_35 = BashOperator(
+        task_id='bash_sleep_35',
+        bash_command='sleep 35'
     )
 
-    bash_sleep_10 = BashOperator(
+    bash_sleep_36 = BashOperator(
         trigger_rule='all_done',
-        task_id='bash_sleep_10',
-        bash_command='sleep 10'
+        task_id='bash_sleep_36',
+        bash_command='sleep 36'
+    )
+
+    bash_go = BashOperator(
+        task_id='bash_go',
+        bash_command='exit 0'
     )
