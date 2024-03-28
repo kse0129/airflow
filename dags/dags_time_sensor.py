8cf1d3bdc6a2dbaafbb0b8dea8335b73f6e52e9e
kse <seongeon1999@naver.com>
2024-03-28 16:22:48 +0900

    modify datetime

--- dags/dags_time_sensor.py
+++ dags/dags_time_sensor.py
@@ -4,8 +4,8 @@ import pendulum
 
 with DAG(
     dag_id='dags_time_sensor',
-    start_date=pendulum.datetime(2024, 4, 1, 0, 0, 0),
-    end_date=pendulum.datetime(2024, 4, 1, 1, 0, 0),
+    start_date=pendulum.datetime(2024, 3, 1, 0, 0, 0),
+    end_date=pendulum.datetime(2024, 3, 1, 1, 0, 0),
     schedule='*/10 * * * *',
     catchup=True
 ) as dag:
